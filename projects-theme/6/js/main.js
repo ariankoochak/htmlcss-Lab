2bb95b51de61343702c8bbea3c0d7bdce184b316
Arian <45326611+ariankoochak@users.noreply.github.com>
2023-08-23 17:40:45 +0330

    complete start page

---
+++ projects-theme/6/js/main.js
@@ -0,0 +1,8 @@
+setTimeout(()=>{
+    document.querySelector(
+        ".first-div"
+    ).innerHTML = `                <div class="title">
+                    <h2>Pro.Beyond.</h2>
+                </div>
+                <img src="./assets/images/first-div-image.jpg" alt="iphone14pro-graphite">`;
+},5000)
