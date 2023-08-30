e22b0482dad89ff24312445349b5471c30fb1f47
Arian <45326611+ariankoochak@users.noreply.github.com>
2023-08-30 03:56:14 +0330

    template-6

--- projects-theme/6/js/main.js
+++
@@ -1,9 +0,0 @@
-setTimeout(()=>{
-    document.querySelector(
-        ".first-div"
-    ).innerHTML = `                <div class="title">
-                    <h2>Pro.Beyond.</h2>
-                </div>
-                <div class="image-holder">
-                <img src="./assets/images/first-div-image.jpg" alt="iphone14pro-graphite"></div>`;
-},5000)
